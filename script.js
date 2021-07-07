250df58d0c676e3f6554512ddef063a0e039b220
gorbachevsergey <gorbachev.sa@icloud.com>
2021-07-07 15:15:56 +0700

    work on burger

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-$('.btnShowNav').click(function() {
-    if ($('.titleNav').css('display') != 'none'){
-        $('.titleNav').css('display', 'none');
-        $('.menuBurgerList').css('display','flex');
-        $('.menuBurger').css('flex-direction','column');
-        $('.menuBurgerListCompany').css('padding',0)
-        $('.menuBurgerListCompany').css('justify-content','center')
-    } else{
-        $('.titleNav').css('display', 'flex')
-        $('.menuBurgerList').css('display','none');
-        $('.menuBurger').css('flex-direction','row');
-    }
-})
+(function() {
+    const burger = document.querySelector('.btnShowNav');
+    const burgerList = document.querySelector('.menuBurgerList');
+    burger.addEventListener('click',() => {
+        burger.classList.toggle('btnShowNavActive');
+        burgerList.classList.toggle('menuBurgerListActive');
+    })
+}());
